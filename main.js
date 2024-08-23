1e3c3d779d696f071054027900fbf1eae44bacd1
smthubakgale <97933623+smthubakgale@users.noreply.github.com>
2024-08-23 15:26:42 +0200

    Update main.js

--- main.js
+++ main.js
@@ -40,20 +40,29 @@ function move(ts)
 //: move 
 $("#mainSVG2").mousemove((ev)=>
 { 
-      if(op == true || om == true)
-      {
-          var oq = (om == true) ? me : $('#mainSVG2').children().last();
-          var x = ev.pageX - $('#mainSVG2').offset().left;
-          var y = ev.pageY - $('#mainSVG2').offset().top;
+    setTimeout(function()
+    {
+       if(op == true || om == true)
+       {
+           var oq = (om == true) ? me : $('#mainSVG2').children().last();
+           var x = ev.pageX - $('#mainSVG2').offset().left;
+           var y = ev.pageY - $('#mainSVG2').offset().top;
 
-          console.log(x , y , typeof(oq));
-          oq.attr("x", x);
-          oq.attr("y", y);
-      }  
+           console.log(x , y , typeof(oq));
+           oq.attr("x", x);
+           oq.attr("y", y);
+       } 
+    } , 300); 
      
 });
 //: drop
-$("#mainSVG2").mouseup((ev) => { drop(ev); });
+$("#mainSVG2").mouseup((ev) => { 
+    setTimeout(function()
+    {
+        console.log("MF");
+        drop(ev); 
+    } , 200);
+});
 $("#mainSVG2").mouseleave((ev) => { drop(ev); });
 
 function drop(ev)
